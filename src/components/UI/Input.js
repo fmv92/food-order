e7335cae49aedc7ce0de731ccaff2efde41aadef
Fernando <fernando.mata.villalobos@gmail.com>
2022-08-27 13:18:07 -0600

    Implemented add functionality to the cart.

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,14 @@
+import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} /> {/*Spread operator makes possible to pass all properties without specifying them explicitly*/}
+      <input ref={ref} {...props.input} />{" "}
+      {/*Spread operator makes possible to pass all properties without specifying them explicitly*/}
     </div>
   );
-};
+});
 
 export default Input;
